b5fb92a020e092b8697aa6ff2cfadcdf004d64e6
Tiffany Ngo <tifngo99@gmail.com>
2022-10-27 08:50:23 -0700

    first commit

--- app.js
+++ app.js
@@ -6,12 +6,20 @@
 
 function makeCall() {
   // Use the fetch api to display some simple user data to the console!
-  url = "https://reqres.in/api/users";  
+ const url = "https://reqres.in/api/users";  
 
   
-  fetch() // call the fetch api with the correct input argument!
-    .then() // convert the returned data into a json using the object's built-in methods.
-    .then() // use the resulting json to display some of our user information in the console.
-    .catch(); // console.log an error message in case something goes wrong.
+  fetch(url) // call the fetch api with the correct input argument! two primary cases where you need a second argument (POST, deleting, update, read and endpoint requires verification)...basically other than GET. First argument is always needed.
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+    }) // convert the returned data into a json using the object's built-in methods. Type of argument it's expecting is a callback function. .then will be called when there is a successful resolved.
+    .then(robert => {
+      const myData = robert.data[2];
+      console.log(myData);
+    }) // use the resulting json to display some of our user information in the console.
+    .catch(console.log); // console.log an error message in case something goes wrong.
 
 };
+
